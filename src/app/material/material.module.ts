1bd1554f32e6e4713369de8075742a243154e41b
Victoria <victoriaaoka@gmail.com>
2020-03-10 20:35:15 +0300

    Add navbar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+
 
 const material = [
-  
+  MatToolbarModule,
+  MatBadgeModule,
+  MatIconModule,
+  MatSelectModule
 ]
 
 @NgModule({
