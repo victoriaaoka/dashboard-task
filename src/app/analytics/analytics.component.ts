e261ef8ed6f3e78e8952d86575ef2beb9ac321a8
Victoria <victoriaaoka@gmail.com>
2020-03-12 01:57:43 +0300

    Add analytics chart and content

--- src/app/analytics/analytics.component.ts
+++ src/app/analytics/analytics.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalyticsComponent implements OnInit {
 
+  public lineChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true,
+  };
+
+  public lineChartLabels = ['Jun 2', 'Jun 3', 'Jun 4', 'Jun 5', 'Jun 6', 'Jun 7', 'Jun8'];
+  public lineChartType = 'line';
+  public lineChartLegend = true;
+
+  public lineChartData = [
+    {data: [1000, 34000, 20000, 10000, 5000, 12000, 25000, 40000], label: 'TOTAL VOLUME'},
+  ];
+
+  public chartColors: any[] = [
+    { 
+      backgroundColor:"rgba(197, 197, 197, 0.07)",
+      borderColor: "rgba(105, 174, 255, 0.47)"
+    }];
+
   constructor() { }
 
   ngOnInit(): void {
