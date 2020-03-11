87ad97a1f67ff67c6d474ece784fb13523e551e1
Victoria <victoriaaoka@gmail.com>
2020-03-12 01:58:26 +0300

    Add settlement chart and content

--- src/app/settlement/settlement.component.ts
+++ src/app/settlement/settlement.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettlementComponent implements OnInit {
 
+  public barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true,
+  };
+
+  public barChartLabels = ['Jun 2', 'Jun 8', 'Jun 14', 'Jun 20', 'Jun 26', 'Jul 1'];
+  public barChartType = 'bar';
+  public barChartLegend = true;
+
+  public barChartData = [
+    {data: [1000, 34000, 20000, 10000, 5000, 12000, 25000, 40000], label: 'TOTAL AVAILABLE BALANCE'},
+  ];
+
+  for(){
+
+  }
+  public chartColors: any[] = [
+    { 
+      backgroundColor:[]
+    }];
+
   constructor() { }
 
   ngOnInit(): void {
